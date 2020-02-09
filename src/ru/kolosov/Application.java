19081a0fcf6d2ad0732a7fd9a40697aff0fa40c5
svetomsk <erere>
2020-02-09 16:06:46 +0300

    first commit

---
+++ src/ru/kolosov/Application.java
@@ -0,0 +1,9 @@
+package ru.kolosov;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Kirill!");
+    }
+
+}
---
+++ src/ru/kolosov/Application2.java
@@ -0,0 +1,9 @@
+package ru.kolosov;
+
+public class Application2 {
+
+    public static void main(String[] args) {
+        System.out.println("Bye Kirill!");
+    }
+
+}
